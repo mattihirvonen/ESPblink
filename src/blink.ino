3e757cef9031c4301c86fb75588a870cfc0c70b5
mattihirvonen <mattihirvonen@hder.fi>
2024-02-28 17:44:24 +0200

    Test ESP32S3 Serial.Print to USB port (not yet work)

--- src/blink.ino
+++ src/blink.ino
@@ -17,6 +17,7 @@ void blink_led( void )
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);                      // wait for a second
+    Serial.println("LED blink");
 }
 
 
